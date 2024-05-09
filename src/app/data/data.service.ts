f9baf40d3b9855de6ed171d9fae44ce4f7ef7b87
Gerard Soler <gerard.soleralmenara@cieep.com>
2024-05-09 10:10:11 +0200

    data.service

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,7 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductosComponent } from '../component/productos/productos.component';
+import { RootInterface } from '../common/productos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  constructor( private http: HttpClient) { }
+
+  getProduct(): Observable<RootInterface>{
+    return this.http.get<RootInterface>('./assets/data/data.json')
+  }
 }
